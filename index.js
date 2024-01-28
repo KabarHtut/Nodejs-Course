cc80e1ad1d91f4ac1ae3c74e29d6022566d51c3e
KabarHtut <kabarhtut21@gmail.com>
2024-01-28 16:11:05 +0630

    Ep-3-global-object

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+// let interval = globalThis.setInterval(function() {
+//     console.log('hello world');
+// }, 1000);
+
+// globalThis.setTimeout(function() {
+//     clearInterval(interval);
+// }, 5000);
+
+// console.log(__dirname);
+console.log(__filename);
